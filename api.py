408d825ef2a46929eab26da33289a8f11d87db6d
bernardoroth <2022boliveira@jpsul.com.br>
2019-10-24 13:20:39 -0300

    Added connection and cursor to api.py

--- api.py
+++ api.py
@@ -1,2 +1,11 @@
-import psycopg2
+import psycopg2 as psql
 import os, time, datetime, re, mimetypes, traceback, base64
+
+conn = psql.connect("dbname=anuario user=postgres")
+c = conn.cursor()
+
+def create_user(username, email, password):
+    pass
+
+cur.close()
+conn.close()
