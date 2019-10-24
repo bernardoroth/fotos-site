d8bb409ad8a64aca8ff401cdec1d398ecdccfbfc
bernardoroth <2022boliveira@jpsul.com.br>
2019-10-24 19:35:09 -0300

    Added types and (proper) password checking (py3.7)

--- api.py
+++ api.py
@@ -1,21 +1,27 @@
 import psycopg2 as psql
 import traceback, base64
 
-while password := input("Senha de 'postgres'> "):
-    if password.replace(" ", "") != "":
+while True:
+    password = input("Senha de 'postgres'> ")
+    try:
         conn = psql.connect(f"dbname=anuario user=postgres password={password}")
         del password
+    except:
+        break
 c = conn.cursor()
 
 def create_user(username, email, password):
     pass
 
-def placeholders(list_of_items):
+def placeholders(items: list) -> str:
     s = []
-    s += ["%s" for _ in list_of_items]
+    s += ["%s" for _ in items]
     return ",".join(s)
 
-def value_lists(data: dict) -> None:
+def value_tuples(data: dict) -> tuple:
+    ''' Function that accepts a dictionary
+    of values and returns two tuples, one
+    for the keys and one for the values '''
     table = data['table']
     data.pop('table')
     cols = []
@@ -23,13 +29,14 @@ def value_lists(data: dict) -> None:
     for cv in data.items():
         cols.append(cv[0])
         vals.append(cv[1])
-    return cols, vals
+    return tuple(cols), tuple(vals)
 
 def insert(data):
     try:
-        cols, vals = value_lists(data)
+        cols, vals = value_tuples(data)
+        value_tuples()
         query = f"INSERT INTO %s ({sput(cols)}) VALUES ({sput(vals)})"
-        values = tuple(cols + vals)
+        values = cols + vals
         cur.execute(query, values)
         conn.commit()
     except:
