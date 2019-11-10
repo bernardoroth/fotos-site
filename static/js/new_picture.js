3a105b5128e8a850db311b353f724f098e92c66e
bernardoroth <2022boliveira@jpsul.com.br>
2019-11-10 14:14:30 -0300

    Form clear with one button (async)

--- static/js/new_picture.js
+++ static/js/new_picture.js
@@ -2,12 +2,24 @@ let formArea = document.querySelector('form');
 let fileInputFunc = document.querySelector('input[type=file]');
 let fileInputLabl = document.querySelector('label[for=file]');
 let resetButton = document.querySelector('button[type=reset]');
+let submitButton = document.querySelector('button[type=submit]');
 
 fileInputFunc.addEventListener('change', e => {
 	let filename = fileInputFunc.files[0].name;
 	fileInputLabl.innerHTML = filename;
 });
 
-resetButton.addEventListener('click', e => {
+/* resetButton.addEventListener('click', e => {
 	fileInputLabl.innerHTML = "Escolha um arquivo";
-});
+}); */
+
+async function resetForm(form) {
+	form.reset();
+	fileInputLabl.innerHTML = "Escolha um arquivo";
+}
+
+submitButton.addEventListener('click', e => {
+	setTimeout(() => {
+		resetForm(formArea);
+	}, 1);
+})
